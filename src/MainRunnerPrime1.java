017cd6e827f1e714b3a45ff6d737fecb870cad00
lwhamilton03 <lucy.hamilton@academytrainee.com>
2018-08-29 13:49:49 +0100

    initialise

---
+++ src/MainRunnerPrime1.java
@@ -0,0 +1,10 @@
+
+public class MainRunnerPrime1 {
+
+		public static void main(String[] args)
+		{
+			PrimeNum num = new PrimeNum(); 
+			num.finall(3000000);
+			num.printPrimes();
+		}
+}
---
+++ src/PrimeNum.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class PrimeNum {
+
+	ArrayList <Integer> primesList = new ArrayList<Integer>();
+	
+	public void finall(int end)
+	{
+		primesList.add(2); 
+		
+		for(int i = 2; i <= end; i++)
+		{
+			boolean add = true; 
+			
+			for(int j = 0; j <= primesList.size() - 1; j++)
+			{
+				if(i % primesList.get(j) == 0)
+				{
+					add = false; 
+					break; 
+				}
+			}
+			if(add == true)
+			{
+			primesList.add(i);
+			}
+		}
+	}
+	
+	public void printPrimes()
+	{
+		for(int i = primesList.size() -1 ; i >= 0; i--)
+		{
+			System.out.println(primesList.get(i));
+		}
+	}
+}
